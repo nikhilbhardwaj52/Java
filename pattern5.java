ba0077055c7a9552c93dd4ba8e353975dbed1fc1
nikhilbhardwaj52 <2906nikhilbhardwaj@gmail.com>
2025-01-10 01:31:38 +0530

    intial

---
+++ pattern5.java
@@ -0,0 +1,24 @@
+public class pattern5 {
+    public static void pattern(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                if((i+j)%2==0)
+                {
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(5);
+        
+    }
+    
+}
